97b9736da52833cc1924d752d4a60866d59786a9
Adam Wright <adam.wright90@yahoo.co.uk>
2021-09-30 20:08:04 +0100

    update

--- src/resources/js/app/splitlines/splitlines.js
+++ src/resources/js/app/splitlines/splitlines.js
@@ -1,12 +1,33 @@
 var FESplitLines = {
     Init: function Init() {
-        $('.js-line-splitting1').splitLines({ tag: '<div class="line-outter"><div class="line-inner">', keepHtml: true });
-        $('.js-line-splitting2').splitLines({ tag: '<div class="line-outter"><div class="line-inner">', keepHtml: true });
+        $('.js-line-splitting').each(function () {
+            var $this = $(this);
+            $this.splitLines({ tag: '<div class="line-outter"><div class="line-inner">', keepHtml: true });
+        });
 
         $(document).ready(function () {
-            var tl = gsap.timeline();
+            $('.line-outter').each(function () {
+                var $this = $(this);
+                if ($this.inView('both', 200)) {
+                    var $line = $this.find('.line-inner');
+                    var tl = gsap.timeline();
 
-            tl.staggerTo('.line-inner', 1, { y: 0, ease: 'power2' }, 0.3);
+                    tl.staggerTo($line, 1, { y: 0, ease: 'power2' }, 0.5);
+                }
+            });
+        });
+    },
+    Scroll: function () {
+        $(window).scroll(function () {
+            $('.line-outter').each(function () {
+                var $this = $(this);
+                if ($this.inView('both', 200)) {
+                    var $line = $this.find('.line-inner');
+                    var tl = gsap.timeline();
+
+                    tl.staggerTo($line, 1, { y: 0, ease: 'power2' }, 0.5);
+                }
+            });
         });
     },
 };
