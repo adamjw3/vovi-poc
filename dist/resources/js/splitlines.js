1c3ea49b1bab0aa8f7becf945a1e2c6a8ea51990
Adam Wright <adam.wright90@yahoo.co.uk>
2021-10-01 10:31:13 +0100

    Add smooth scroll

--- dist/resources/js/splitlines.js
+++ dist/resources/js/splitlines.js
@@ -1,16 +1,12 @@
 'use strict';
 
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
-
 var FESplitLines = {
     Init: function Init() {
-        this.Scroll();
         this.AddLines();
-        this.Intersection();
-        this.FadeIn();
+        this.Scroll();
     },
     Scroll: function Scroll() {
-        var scroll = new LocomotiveScroll({
+        new LocomotiveScroll({
             el: document.querySelector('[data-scroll-container]'),
             smooth: true
         });
@@ -18,37 +14,7 @@ var FESplitLines = {
     AddLines: function AddLines() {
         $('.js-line-splitting').each(function () {
             var $this = $(this);
-            $this.splitLines({ tag: '<div class="line-outter"><div class="line-inner">', keepHtml: true });
-        });
-    },
-    FadeIn: function FadeIn() {
-        $('body').css({ opacity: 1 });
-    },
-    Intersection: function Intersection() {
-        var el = [].concat(_toConsumableArray(document.querySelectorAll('.line-outter')));
-
-        var options = {
-            rootMargin: '-10%',
-            threshold: 0.0
-        };
-
-        var observer = new IntersectionObserver(showItem, options);
-
-        function showItem(entries) {
-            console.log(entries);
-            entries.forEach(function (entry) {
-                if (entry.isIntersecting) {
-                    entry.target.children[0].classList.add('is-animate');
-                }
-            });
-        }
-
-        el.forEach(function (item) {
-            var newString = '';
-            //let itemText = item.children[0].innerText().split();
-            //itemText.map((letter) => (newString += letter = ' ' ? `<span class="gap"></span>` : `<span>${letter}</span>`));
-
-            observer.observe(item);
+            $this.splitLines({ tag: '<div class="line-outter"><div class="line-inner" data-scroll>', keepHtml: true });
         });
     }
 };
--- src/resources/js/app/splitlines/splitlines.js
+++ src/resources/js/app/splitlines/splitlines.js
@@ -1,12 +1,10 @@
 var FESplitLines = {
     Init: function Init() {
-        this.Scroll();
         this.AddLines();
-        this.Intersection();
-        this.FadeIn();
+        this.Scroll();
     },
     Scroll: function () {
-        const scroll = new LocomotiveScroll({
+        new LocomotiveScroll({
             el: document.querySelector('[data-scroll-container]'),
             smooth: true,
         });
@@ -14,37 +12,7 @@ var FESplitLines = {
     AddLines: function () {
         $('.js-line-splitting').each(function () {
             var $this = $(this);
-            $this.splitLines({ tag: '<div class="line-outter"><div class="line-inner">', keepHtml: true });
-        });
-    },
-    FadeIn: function () {
-        $('body').css({ opacity: 1 });
-    },
-    Intersection: function () {
-        let el = [...document.querySelectorAll('.line-outter')];
-
-        let options = {
-            rootMargin: '-10%',
-            threshold: 0.0,
-        };
-
-        let observer = new IntersectionObserver(showItem, options);
-
-        function showItem(entries) {
-            console.log(entries);
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    entry.target.children[0].classList.add('is-animate');
-                }
-            });
-        }
-
-        el.forEach((item) => {
-            let newString = '';
-            //let itemText = item.children[0].innerText().split();
-            //itemText.map((letter) => (newString += letter = ' ' ? `<span class="gap"></span>` : `<span>${letter}</span>`));
-
-            observer.observe(item);
+            $this.splitLines({ tag: '<div class="line-outter"><div class="line-inner" data-scroll>', keepHtml: true });
         });
     },
 };
